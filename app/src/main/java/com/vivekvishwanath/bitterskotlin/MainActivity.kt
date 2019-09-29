f37c454e7a00c0a6d871ee4e7a42869900bc954a
VivekV95 <vivekv210@gmail.com>
2019-09-28 22:15:34 -0500

    Add initial necessary dependencies

--- app/src/main/java/com/vivekvishwanath/bitterskotlin/MainActivity.kt
+++ app/src/main/java/com/vivekvishwanath/bitterskotlin/MainActivity.kt
@@ -2,11 +2,13 @@ package com.vivekvishwanath.bitterskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
