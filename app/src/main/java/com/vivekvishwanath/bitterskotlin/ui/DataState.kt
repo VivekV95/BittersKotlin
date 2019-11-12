874dedf7ca6bcdc649520de141bf41b018cc1713
vivekv95 <vivekv210@gmail.com>
2019-11-11 19:59:38 -0600

    Write DataState class to manage data, loading, and error states

---
+++ app/src/main/java/com/vivekvishwanath/bitterskotlin/ui/DataState.kt
@@ -0,0 +1,28 @@
+package com.vivekvishwanath.bitterskotlin.ui
+
+data class DataState <T>(
+    var message: Event<String>? = null,
+    var loading: Boolean = false,
+    var data: Event<T>? = null
+) {
+    companion object {
+
+        fun <T> error(message: String): DataState<T> =
+            DataState(
+                message = Event(message),
+                loading = false,
+                data = null)
+
+        fun <T> loading(isLoading: Boolean): DataState<T> =
+            DataState(
+                loading = isLoading
+            )
+
+        fun <T> data(message: String?, data: T? = null): DataState<T> =
+            DataState(
+                message = Event.messageEvent(message),
+                loading = false,
+                data = Event.dataEvent(data)
+            )
+    }
+}
