f34555055637d0d60b1a1d456c21fadc1a8e01e1
vivekv95 <vivekv210@gmail.com>
2019-11-11 20:30:58 -0600

    Create generic BaseViewModel for all ViewModels in app

--- app/src/main/java/com/vivekvishwanath/bitterskotlin/ui/BaseViewModel.kt
+++ app/src/main/java/com/vivekvishwanath/bitterskotlin/ui/BaseViewModel.kt
@@ -1,1 +1,39 @@
 package com.vivekvishwanath.bitterskotlin.ui
+
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel<StateEvent, ViewState> : ViewModel() {
+
+    private val _stateEvent: MutableLiveData<StateEvent> = MutableLiveData()
+    protected val _viewState: MutableLiveData<ViewState> = MutableLiveData()
+
+    val viewState: LiveData<ViewState>
+        get() = _viewState
+
+    val dataState: LiveData<DataState<ViewState>> =
+        Transformations
+            .switchMap(_stateEvent) { stateEvent ->
+                stateEvent?.let {
+                    handleStateEvent(stateEvent)
+                }
+            }
+
+    fun setStateEvent(event: StateEvent) {
+        _stateEvent.value = event
+    }
+
+    fun getCurrentViewStateOrNew(): ViewState {
+        val value = viewState.value?.let { 
+            it
+        }?: initNewViewState()
+        return value
+    }
+
+    abstract fun initNewViewState(): ViewState
+
+    abstract fun handleStateEvent(stateEvent: StateEvent): LiveData<DataState<ViewState>>
+}
