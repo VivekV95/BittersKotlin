4b6e3abac61a11e9117c645a87fa50064135652a
vivekv95 <vivekv210@gmail.com>
2019-11-18 22:40:39 -0600

    Create CocktailListAdapter for RV to display Cocktails

--- app/src/main/java/com/vivekvishwanath/bitterskotlin/di/main/MainModule.kt
+++ app/src/main/java/com/vivekvishwanath/bitterskotlin/di/main/MainModule.kt
@@ -1,6 +1,7 @@
 package com.vivekvishwanath.bitterskotlin.di.main
 
 import android.app.Application
+import com.bumptech.glide.Glide
 import com.vivekvishwanath.bitterskotlin.di.scope.MainScope
 import com.vivekvishwanath.bitterskotlin.network.CocktailDbService
 import com.vivekvishwanath.bitterskotlin.network.CocktailDbServiceWrapper
@@ -17,5 +18,10 @@ class MainModule {
         @Provides
         @JvmStatic
         fun provideCocktailDbService(application: Application) = CocktailDbServiceWrapper(application)
+
+        @MainScope
+        @Provides
+        @JvmStatic
+        fun provideGlideInstance(application: Application) = Glide.with(application)
     }
 }
